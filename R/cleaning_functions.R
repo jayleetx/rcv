afbb604de912750161401c5baa80384aa7046797
Jay Lee <jaylee@reed.edu>
2017-05-26 17:43:34 -0700

    Renamed functions, tried to automate format detection

--- R/cleaning_functions.R
+++ R/cleaning_functions.R
@@ -1,7 +1,19 @@
-read_ballot <- function(data, format) {
-  if (format == "California") {
-  data %>%
-    separate(X1, into = c("contest_id",
+#' Separates single column ballot data frames.
+#'
+#' Takes a data frame of a single column (i.e.
+#' sf_bos_ballot) and splits it into usable columns.
+#'
+#' @param data A data frame containing the ballot image data
+#' @param format Which RCV format the ballot image is in
+#' @return A data frame with multiple columns
+#' @examples
+#' read_ballot(data = sf_bos_ballot, format = "California")
+#' @importFrom dplyr %>%
+
+label_ballot <- function(data) {
+  if (nchar(data[1,1]) == 45) {
+    data %>%
+      tidyr::separate(X1, into = c("contest_id",
                           "pref_voter_id",
                           "serial_number",
                           "tally_type_id",
@@ -10,14 +22,49 @@ read_ballot <- function(data, format) {
                           "candidate_id",
                           "over_vote",
                           "under_vote"),
-             sep = c(7,16,23,26,33,36,43,44))
+               sep = c(7,16,23,26,33,36,43,44))
   }
 
-  else if (format == "Cambridge") {
+  else if(tools::file_ext(data) == "csv" & nchar(data[1,1]) == 14) {
+    data
+    # Hey Jay write this part next week when you hear back from Theo
+  }
+
+  else stop('incompatible ballot format')
+
+}
+
+
+#' Separates single column ballot data frames.
+#'
+#' Takes a data frame of a single column (i.e.
+#' sf_bos_ballot) and splits it into usable columns.
+#'
+#' @param data A data frame containing the ballot image data
+#' @param format Which RCV format the ballot image is in
+#' @return A data frame with multiple columns
+#' @examples
+#' read_ballot(data = sf_bos_ballot)
+#' @importFrom dplyr %>%
+
+label_lookup <- function(data) {
+  if (nchar(data[1,1]) == 83) {
     data %>%
-      separate()
+      tidyr::separate(X1, into = c("record_type",
+                                   "id",
+                                   "description",
+                                   "list_order",
+                                   "condidates_contest_id",
+                                   "is_writein",
+                                   "is_provisional"),
+                      sep = c(10,17,67,74,81,82))
   }
 
-  else stop('unknown ballot format')
+  else if(tools::file_ext(data) == "csv" & grep(".TITLE", cambridge_lookup[1,1]) == 1) {
+    data
+    # hey Jay write this part next week when you hear back from Theo
+  }
+
+  else stop('incompatible ballot format')
 
 }
