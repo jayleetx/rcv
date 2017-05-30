22ac1de7192897c45932b25b8552a1e9da10748d
Jay Lee <jaylee@reed.edu>
2017-05-30 12:20:50 -0700

    Cleaned up formatting changes

--- R/cleaning_functions.R
+++ R/cleaning_functions.R
@@ -1,52 +1,51 @@
+#1234567890123456789012345678901234567890123456789012345678901234567890123456789
+
 #' Separates single column ballot data frames.
 #'
-#' Takes a data frame of a single column (i.e.
-#' sf_bos_ballot) and splits it into usable columns.
+#' Takes a data frame of a single column (i.e. sf_bos_ballot) and splits it
+#' into usable named columns.
 #'
 #' @param data A data frame containing the ballot image data
-#' @param format Which RCV format the ballot image is in
 #' @return A data frame with multiple columns
 #' @examples
-#' read_ballot(data = sf_bos_ballot, format = "California")
+#' label_ballot(data = sf_bos_ballot)
 #' @importFrom dplyr %>%
 
 label_ballot <- function(data) {
   if (nchar(data[1,1]) == 45) {
     data %>%
       tidyr::separate(X1, into = c("contest_id",
-                          "pref_voter_id",
-                          "serial_number",
-                          "tally_type_id",
-                          "precinct_id",
-                          "vote_rank",
-                          "candidate_id",
-                          "over_vote",
-                          "under_vote"),
-               sep = c(7,16,23,26,33,36,43,44))
+                                   "pref_voter_id",
+                                   "serial_number",
+                                   "tally_type_id",
+                                   "precinct_id",
+                                   "vote_rank",
+                                   "candidate_id",
+                                   "over_vote",
+                                   "under_vote"),
+                      sep = c(7,16,23,26,33,36,43,44))
   }
 
-  else if(tools::file_ext(data) == "csv" & nchar(data[1,1]) == 14) {
-    data
-    # Hey Jay write this part next week when you hear back from Theo
-  }
+#  else if(tools::file_ext(data) == "csv" & nchar(data[1,1]) == 14) {
+#    data
+#  }
+# Hey Jay write this part next week when you hear back from Theo
 
   else stop('incompatible ballot format')
 
 }
 
 
-#' Separates single column ballot data frames.
+#' Separates single column master lookup data frames.
 #'
-#' Takes a data frame of a single column (i.e.
-#' sf_bos_ballot) and splits it into usable columns.
+#' Takes a data frame of a single column (i.e. sf_bos_lookup) and splits it
+#' into usable columns.
 #'
-#' @param data A data frame containing the ballot image data
-#' @param format Which RCV format the ballot image is in
+#' @param data A data frame containing the master lookup data
 #' @return A data frame with multiple columns
 #' @examples
-#' read_ballot(data = sf_bos_ballot)
+#' label_lookup(data = sf_bos_lookup)
 #' @importFrom dplyr %>%
-
 label_lookup <- function(data) {
   if (nchar(data[1,1]) == 83) {
     data %>%
@@ -60,11 +59,8 @@ label_lookup <- function(data) {
                       sep = c(10,17,67,74,81,82))
   }
 
-  else if(tools::file_ext(data) == "csv" & grep(".TITLE", cambridge_lookup[1,1]) == 1) {
-    data
-    # hey Jay write this part next week when you hear back from Theo
-  }
-
   else stop('incompatible ballot format')
 
 }
+
+
