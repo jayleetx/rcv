7c3b10bb99015329e2379c063303043511c2758d
Jay Lee <jaylee@reed.edu>
2017-05-30 14:49:59 -0700

    Moved sandbox

--- sandbox/characterise.R
+++ sandbox/characterise.R
@@ -80,3 +80,18 @@ ballot_tidy <- function(rawimage, key, df) {
 
   return(BallotImage)
 }
+
+
+characterise <- function(ballot, lookup) {
+  for (name in colnames(ballot)) {
+    a <- filter(lookup, trimws(record_type) == name)
+    for (index in seq_along(nrow(a))) {
+    ballot$name[ballot$name %in% a$id[index]] <- a$description[index]
+    }
+  }
+}
+
+
+#for (id in 1:nrow(df2)) {
+#  df1$x2[df1$x1 %in% df2$x1[id]] <- df2$x2[id]
+#}
