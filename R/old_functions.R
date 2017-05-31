9ffd0c76aeb82f9ee43a3e3570d873faaec59800
Matthew Yancheff <yanchefm@reed.edu>
2017-05-31 12:44:14 -0700

    got new version of tally function working

--- R/old_functions.R
+++ R/old_functions.R
@@ -179,3 +179,46 @@ old_rcv_plot <- function(rcvresults) {
   }
   else {return(print("Too few candidates to create plot."))}
 }
+
+
+#' First attempt at getting RCV results with a prebuilt dataframe
+#'
+#' @param image A dataframe containing rcv election data
+#' @param rcvcontest The election to calculate results for
+#' @return A dataframe that contains vote tallies
+#' @examples
+#' rcv_tally_test(image = "sf_bos_clean", rcvcontest = "Board of Supervisors, District 1")
+#' @export
+rcv_tally_test <- function(image, rcvcontest) {
+  rcv <- image %>%
+    filter(contest == rcvcontest) %>%
+    mutate(candidate = ifelse(is.na(candidate), "NA", candidate)) # move this step to ballot cleaning
+  n.cand <- length(unique(rcv$candidate))
+  results <- data.frame(matrix(rep(NA, n.cand*(n.cand-2)), nrow = n.cand))
+  row.names(results) <- unique(rcv$candidate)
+  roundnames <- c()
+  for (i in 1:(n.cand - 2)) {
+    roundnames <- append(roundnames, paste0("round", i))
+  }
+  colnames(results) <- roundnames
+  elim <- data.frame(candidate = character(), total = integer())
+  for (i in 1:(n.cand - 2)) {
+    round <- rcv %>%
+      filter(!(candidate %in% elim$candidate)) %>%
+      group_by(pref_voter_id) %>%
+      filter(vote_rank == min(vote_rank)) %>%
+      ungroup() %>%
+      group_by(candidate) %>%
+      summarise(total = n())
+    round <- rbind(round, elim)
+    results[, i] <- round %>% select(total)
+    loser <- round %>%
+      arrange(total) %>%
+      filter(candidate != "NA") %>%
+      filter(!(candidate %in% round)) %>%
+      head(n = 1) %>%
+      mutate(total = NA)
+    elim <- rbind(elim, loser)
+  }
+  return(results)
+}
--- sandbox/rcv_tally.R
+++ sandbox/rcv_tally.R
@@ -1,43 +1,8 @@
 # tibbles mess this code up, fix namespace and imports
 
-rcv_tallyv1 <- function(image, rcvcontest) {
-  rcv <- image %>%
-    filter(contest == rcvcontest) %>%
-    mutate(candidate = ifelse(is.na(candidate), "NA", candidate)) # move this step to ballot cleaning
-  n.cand <- length(unique(rcv$candidate))
-  results <- data.frame(matrix(rep(NA, n.cand*(n.cand-2)), nrow = n.cand))
-  row.names(results) <- unique(rcv$candidate)
-  roundnames <- c()
-  for (i in 1:(n.cand - 2)) {
-    roundnames <- append(roundnames, paste0("round", i))
-  }
-  colnames(results) <- roundnames
-  elim <- data.frame(candidate = character(), total = integer())
-  for (i in 1:(n.cand - 2)) {
-    round <- rcv %>%
-      filter(!(candidate %in% elim$candidate)) %>%
-      group_by(pref_voter_id) %>%
-      filter(vote_rank == min(vote_rank)) %>%
-      ungroup() %>%
-      group_by(candidate) %>%
-      summarise(total = n())
-    round <- rbind(round, elim)
-    results[, i] <- round %>% select(total)
-    loser <- round %>%
-      arrange(total) %>%
-      filter(candidate != "NA") %>%
-      filter(!(candidate %in% round)) %>%
-      head(n = 1) %>%
-      mutate(total = NA)
-    elim <- rbind(elim, loser)
-  }
-  return(results)
-}
-
-
-rcv_tally <- function(image, contest) {
+rcv_tally <- function(image, rcvcontest) {
   ballot <- image %>%
-    filter(contest == contest) %>%
+    filter(contest == rcvcontest) %>%
     mutate(candidate = ifelse(is.na(candidate), "NA", candidate)) %>%
     select(pref_voter_id, vote_rank, candidate)
 
@@ -52,45 +17,39 @@ rcv_tally <- function(image, contest) {
 
   elim <- data.frame(candidate = character())
 
-  round1 <- ballot %>%
-    group_by(pref_voter_id) %>%
-    filter(vote_rank == min(vote_rank)) %>%
-    ungroup() %>%
-    group_by(candidate) %>%
-    summarise(total = n())
+  for (j in 1:(n.cand - 2)) {
+    if (j != 1) {
+      transfers <- ballot %>%
+        filter(vote_rank == min(vote_rank), candidate %in% loser) %>%
+        select(pref_voter_id)
+    } else transfers <- ballot %>% select(pref_voter_id)
 
-  round1 <- data.frame(round1)
-  row.names(round1) <- round1$candidate
+    ballot <- ballot %>%
+      filter(!(candidate %in% elim$candidate))
 
-  for(i in unique(round1$candidate)) {
-    results[i, 1] <- round1[i, 2]
+    round <- ballot %>%
+      filter(pref_voter_id %in% transfers$pref_voter_id) %>%
+      group_by(pref_voter_id) %>%
+      filter(vote_rank == min(vote_rank)) %>%
+      ungroup() %>%
+      group_by(candidate) %>%
+      summarise(total = n())
+
+    round <- data.frame(round)
+    row.names(round) <- round$candidate
+
+    for (i in unique(round$candidate)) {
+      if (j !=1) {
+        results[i, j] <- (results[i, j-1] + round[i, 2])
+      } else results [i, j] <- round[i, 2]
     }
 
-  loser <- round1 %>%
-    filter(candidate != "NA") %>%
-    filter(round1 == min(round1)) %>%
-    select(candidate)
+    loser <- round %>%
+      filter(candidate != "NA") %>%
+      filter(total == min(total)) %>%
+      select(candidate)
 
-  elim <- rbind(elim, loser)
-
-  transfers <- ballot %>%
-    filter(vote_rank == min(vote_rank), candidate %in% loser) %>%
-    select(pref_voter_id)
-
-  round2 <- ballot %>%
-    filter(pref_voter_id %in% transfers$pref_voter_id) %>%
-    filter(!(candidate %in% elim$candidate)) %>%
-    group_by(pref_voter_id) %>%
-    filter(vote_rank == min(vote_rank)) %>%
-    ungroup() %>%
-    group_by(candidate) %>%
-    summarise(round2 = n())
-
-  round2 <- data.frame(round2)
-  row.names(round2) <- round2$candidate
-
-  for(i in unique(round2$candidate)) {
-    results[i, 2] <- (results[i, 1] + round2[i, 2])
+    elim <- rbind(elim, loser)
   }
 
   return(results)
