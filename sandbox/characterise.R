6460578e6e50e0819ebaa1c47a497ee28998a273
Jay Lee <jaylee@reed.edu>
2017-05-31 10:53:26 -0700

    More work on calling column names

--- sandbox/characterise.R
+++ sandbox/characterise.R
@@ -1,8 +1,8 @@
 characterise <- function(ballot, lookup) {
-  for (name in colnames(ballot)) {
-    a <- filter(lookup, trimws(record_type) == name)
-    for (index in seq_along(nrow(a))) {
-    ballot$name[ballot$name %in% a$id[index]] <- a$description[index]
+  for (i in seq_along(colnames(ballot))) {
+    a <- dplyr::filter(lookup, trimws(record_type) == colnames(ballot)[i])
+    for (j in seq_along(nrow(a))) {
+    ballot$i[ballot$i %in% a[j, "id"]] <- a[j, "description"]
     }
   }
 }
@@ -11,3 +11,7 @@ characterise <- function(ballot, lookup) {
 #for (id in 1:nrow(df2)) {
 #  df1$x2[df1$x1 %in% df2$x1[id]] <- df2$x2[id]
 #}
+
+# a[j, "description"] -> ballot %>%
+#   select(i) %>%
+#   filter(i %in% a[j, "id"])
