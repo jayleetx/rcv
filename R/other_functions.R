a0ea7092cf993448681c4258855c49788d24135e
Jay Lee <jaylee@reed.edu>
2017-06-05 13:23:25 -0700

    Edited documentation to fix travis build

--- R/other_functions.R
+++ R/other_functions.R
@@ -1,6 +1,6 @@
 #' Alters ballot dataframe to have each row correspond to a single voter
 #'
-#' @param x A tidy dataframe that comes from ballot_tidy()
+#' @param clean A tidy dataframe that comes from ballot_tidy()
 #' @return A dataframe that can be easily read and understood by humans
 #' @examples
 #' readable(sf_bos_clean)
