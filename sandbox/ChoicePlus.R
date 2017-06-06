c14fa55f416bdf057d7cda0f19ec9cbb17fa8981
Jay Lee <jaylee@reed.edu>
2017-06-06 11:50:57 -0700

    Finished "label" for ChoicePlus method

---
+++ sandbox/ChoicePlus.R
@@ -0,0 +1,27 @@
+#read
+data <- readr::read_csv(path,
+                 col_names = paste0("V",
+                                    seq_len(max(count.fields(path,
+                                                             sep = ',')))))
+
+#label
+a <- data %>%
+  tidyr::separate(V1, into = c("a",
+                               "ward",
+                               "precinct",
+                               "b",
+                               "unique"),
+                  sep = c(2,4,6,10),
+                  remove = T) %>%
+  select(-a, -b) %>%
+  tidyr::separate(V4, into = c("a", "1"),
+                  sep = 3,
+                  remove = T) %>%
+  tidyr::unite(col = "contest",
+               V3, a,
+               sep = ", ") %>%
+  mutate(`1` = replace(`1`, which(`1` == ""), NA))
+
+colnames(a) <- c("ward","precinct", "unique", "style", "contest", as.character(c(1:(ncol(a)-5))))
+
+return(a)
