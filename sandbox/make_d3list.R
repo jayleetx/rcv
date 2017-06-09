c2dc4b34991ec840ae0588812b382be418897bba
Matthew Yancheff <yanchefm@reed.edu>
2017-06-09 12:53:22 -0700

    added package calls via double colons

--- sandbox/make_d3list.R
+++ sandbox/make_d3list.R
@@ -1,41 +1,41 @@
 make_d3list <- function(results) {
 
   names <- results %>%
-    gather(key = round, value = count, 2:ncol(results)) %>%
-    filter(!is.na(count))
+    tidyr::gather(key = round, value = count, 2:ncol(results)) %>%
+    dplyr::filter(!is.na(count))
 
   names_id <- names %>%
-    rownames_to_column("cand_id") %>%
-    mutate(cand_id = as.numeric(cand_id) - 1) %>%
-    select(cand_id, candidate, round) %>%
-    spread(key = round, value = cand_id, fill = NA) %>%
-    select(2:ncol(results)) %>%
-    arrange(.[, 1])
+    tibble::rownames_to_column("cand_id") %>%
+    dplyr::mutate(cand_id = as.numeric(cand_id) - 1) %>%
+    dplyr::select(cand_id, candidate, round) %>%
+    tidyr::spread(key = round, value = cand_id, fill = NA) %>%
+    dplyr::select(2:ncol(results)) %>%
+    dplyr::arrange(.[, 1])
 
-  names <- names %>% select(candidate)
+  names <- names %>% dplyr::select(candidate)
 
   for (j in 3:ncol(results)) {
     temp <- data.frame(results[, 1], results[, j] - results[, j-1])
     colnames(temp) <- c("candidate", paste0("transfer", j-2))
     if (j == 3) {transfers <- temp
-    } else transfers <- left_join(transfers, temp, by = "candidate")
+    } else transfers <- dplyr::left_join(transfers, temp, by = "candidate")
   }
 
-  transfers <- transfers %>% select(-candidate)
+  transfers <- transfers %>% dplyr::select(-candidate)
 
-  round_totals <- results %>% select(-candidate)
+  round_totals <- results %>% dplyr::select(-candidate)
 
   source <- data.frame(source = numeric())
 
   for (j in 1:(ncol(names_id) - 1)) {
     source_temp <- data.frame(source = numeric())
     source_temp1 <- names_id %>%
-      filter(!is.na(names_id[, j+1])) %>%
-      select(j)
+      dplyr::filter(!is.na(names_id[, j+1])) %>%
+      dplyr::select(j)
     colnames(source_temp1) <- c("source")
     source_temp2 <- names_id %>%
-      filter(is.na(names_id[, j+1])) %>%
-      select(j)
+      dplyr::filter(is.na(names_id[, j+1])) %>%
+      dplyr::select(j)
     source_temp2 <- data.frame(source = rep(source_temp2[1,1], nrow(source_temp1)))
     source_temp <- rbind(source_temp1, source_temp2)
     source <- rbind(source, source_temp)
@@ -45,8 +45,8 @@ make_d3list <- function(results) {
 
   for (j in 2:ncol(names_id)) {
     target_temp <- names_id %>%
-      filter(!is.na(names_id[, j])) %>%
-      select(j)
+      dplyr::filter(!is.na(names_id[, j])) %>%
+      dplyr::select(j)
     colnames(target_temp) <- c("target")
     target <- rbind(target, target_temp, target_temp)
   }
@@ -55,12 +55,12 @@ make_d3list <- function(results) {
 
   for (j in 1:ncol(transfers)) {
     temp_round <- round_totals %>%
-      filter(!is.na(round_totals[, j+1])) %>%
-      select(j)
+      dplyr::filter(!is.na(round_totals[, j+1])) %>%
+      dplyr::select(j)
     colnames(temp_round) <- c("value")
     temp_transfers <- transfers %>%
-      filter(!is.na(transfers[, j])) %>%
-      select(j)
+      dplyr::filter(!is.na(transfers[, j])) %>%
+      dplyr::select(j)
     colnames(temp_transfers) <- c("value")
     temp_value <- rbind(temp_round, temp_transfers)
     value <- rbind(value, temp_value)
