b985ae482ec10225584f526788e58f3c6d839cb9
Jay Lee <jaylee@reed.edu>
2019-04-05 11:02:49 -0700

    Updated tally function to deal with the no-NA case, added comments

--- R/tabulation.R
+++ R/tabulation.R
@@ -16,54 +16,51 @@
 #' rcv_tally(sf_bos_clean, rcvcontest = "Board of Supervisors, District 7")
 #' rcv_tally(cambridge_clean, n_winner = )
 #' @export
-rcv_tally <- function(image, rules = c('rcv','stv'), n_winners = 1, rcvcontest) {
-  contest <- candidate <- pref_voter_id <- vote_rank <- n <- total <- NULL
+rcv_tally <- function (image, rcvcontest) {
+  contest <- candidate <- pref_voter_id <- vote_rank <- n <- total <- ballot <- NULL
   unique.ballot.candidate. <- . <- NULL
+  # filter for the specified contest
   if (!(missing(rcvcontest))) {
     image <- image %>% dplyr::filter(contest == rcvcontest)
   }
-  ballot <- image %>%
-    dplyr::filter(stringr::str_detect(candidate, "=") %in% c(F, NA)) %>%
-    dplyr::mutate(candidate = ifelse(is.na(candidate),
-                                     "NA",
-                                     candidate)) %>%
-    dplyr::select(pref_voter_id,
-                  vote_rank,
-                  candidate)
+  # drop overvotes, make NA a character
+  ballot <- image %>% dplyr::filter(stringr::str_detect(candidate, "=") %in% c(F, NA)) %>%
+    dplyr::mutate(candidate = ifelse(is.na(candidate),"NA", candidate)) %>%
+    dplyr::select(pref_voter_id, vote_rank, candidate)
 
-  n_cand <- length(unique(ballot$candidate))
-  n_round <- n_cand - 1 - n_winners
-  results <- data.frame(matrix(rep(NA, n_cand*n_round),
-                               nrow = n_cand))
-  roundnames <- rep(NA, n_round)
-  for (i in 1:n_round) {
-    roundnames[i] <- paste0("round", i)
+  # set up empty results df
+  n.cand <- length(unique(c(ballot$candidate, "NA")))
+  results <- data.frame(matrix(rep(NA, n.cand * (n.cand - 2)),
+                               nrow = n.cand))
+  row.names(results) <- unique(c(ballot$candidate, "NA"))
+  roundnames <- c()
+  for (i in 1:(n.cand - 2)) {
+    roundnames <- append(roundnames, paste0("round", i))
   }
   colnames(results) <- roundnames
-  row.names(results) <- unique(ballot$candidate)
 
+  # run the algorithm
+
+  # make blank df for dropped candidates
   elim <- data.frame(candidate = character())
-
-  for (j in 1:n_round) {
+  for (j in 1:(n.cand - 2)) {
+    # if there are >= 3 cands left, select everybody who voted for an eliminated candidate
     if (j >= 2) {
       transfers <- ballot %>%
-        dplyr::filter(vote_rank == min(vote_rank),
-                      candidate %in% loser) %>%
+        dplyr::filter(vote_rank == min(vote_rank), candidate %in% loser) %>%
         dplyr::select(pref_voter_id)
       a <- 0
-    } else transfers <- ballot %>% dplyr::select(pref_voter_id)
-
+    } # else select everybody
+    else transfers <- ballot %>% dplyr::select(pref_voter_id)
     if (nrow(transfers) == 0) {
       transfers <- ballot %>% dplyr::select(pref_voter_id)
       a <- 1
     }
-
-    ballot <- ballot %>%
-      dplyr::filter(!(candidate %in% elim$candidate))
-
+    # drop the eliminated cands, list remaining
+    ballot <- ballot %>% dplyr::filter(!(candidate %in% elim$candidate))
     candidates <- data.frame(unique(ballot$candidate)) %>%
       dplyr::transmute(candidate = as.character(unique.ballot.candidate.))
-
+    # get new preferences for every voter, count up votes for candidates
     round <- ballot %>%
       dplyr::filter(pref_voter_id %in% transfers$pref_voter_id) %>%
       dplyr::group_by(pref_voter_id) %>%
@@ -72,40 +69,33 @@ rcv_tally <- function(image, rules = c('rcv','stv'), n_winners = 1, rcvcontest) 
       dplyr::group_by(candidate) %>%
       dplyr::summarise(total = n()) %>%
       data.frame() %>%
-      dplyr::right_join(candidates,
-                 by = c("candidate")) %>%
-        dplyr::mutate(total = ifelse(is.na(total), 0, total))
-
+      dplyr::right_join(candidates, by = c("candidate")) %>%
+      dplyr::mutate(total = ifelse(is.na(total), 0, total))
+    # write data into the results df
     row.names(round) <- round$candidate
-
     for (i in unique(round$candidate)) {
       if (j >= 2) {
         if (a != 1) {
-          round[i, 2] <- (results[i, j-1] + round[i, 2])
+          round[i, 2] <- (results[i, j - 1] + round[i,
+                                                    2])
         }
       }
-
-      results [i, j] <- round[i, 2]
-
+      results[i, j] <- round[i, 2]
     }
-
-    loser <- round %>%
-      dplyr::filter(candidate != "NA") %>%
-      dplyr::filter(total == min(total)) %>%
-      dplyr::select(candidate)
-
+    # grab the loser, add to eliminated
+    loser <- round %>% dplyr::filter(candidate != "NA") %>%
+      dplyr::filter(total == min(total)) %>% dplyr::select(candidate)
     elim <- rbind(elim, loser)
   }
+  # end of for loop
 
-  results %>%
-    dplyr::select(which(as.vector(colSums(is.na(.)) < (nrow(.) - 1)))) %>%
+  # arrange results df for viewing
+  results %>% dplyr::select(which(as.vector(colSums(is.na(.)) < (nrow(.) - 1)))) %>%
     tibble::rownames_to_column("candidate") %>%
-    dplyr::arrange(candidate == "NA",
-                   rowSums(is.na(.)),
-                   dplyr::desc(.[ ,ncol(.)])) %>%
+    dplyr::arrange(candidate == "NA", rowSums(is.na(.)),
+                   dplyr::desc(.[, ncol(.)])) %>%
     tibble::column_to_rownames("candidate") %>%
     add_exhausted()
-
 }
 
 
